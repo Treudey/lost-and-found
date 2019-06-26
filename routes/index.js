8c98d588f82feedd0180b5c954baebb9a2f15ef3
Brijesh Patel <patelbrijesh14@gmail.com>
2019-06-25 22:27:55 -0400

    updated index.js api routes

--- routes/index.js
+++ routes/index.js
@@ -4,8 +4,9 @@ const apiRoutes = require("./api");
 
 // Send every other request to the React app
 // Define any API routes before this runs
-router.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+app.get('*', function (req, res) {
+  const index = path.join(__dirname, 'build', 'index.html');
+  res.sendFile(index);
 });
 
 // API Routes
