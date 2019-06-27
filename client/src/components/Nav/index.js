d77b99314dc91541d6c4e05d7a12cdf54c9a8390
amandat <amandatome@me.com>
2019-06-27 18:47:19 -0400

    Navbar

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -1,12 +1,30 @@
 import React from "react"
 // import "./Navbar.css";
 
-// Navbar component
-const Navbar = props => (
-  <nav className="navbar fixed-top navbar-dark p-3">
-    <span className="navbar-brand"><a href="/">Finders Keeper</a></span>
-    <span className="navbar-text text-white">{props.loginMessage}</span>
-  </nav>
-);
+function Navbar() {
+    return (
+        <nav className="navbar navbar-expand-lg">
+            <a className="navbar-brand" href="/">
+                <h2 className="text-white">Finders Keepers</h2>
+            </a>
+            <div id="navbarNav">
+                <ul className="navbar-nav">
+                    <li className="nav-item" id="home">
+                        <a className="nav-link" href="/">Home</a>
+                    </li>
+                    <li className="nav-item" id="report">
+                        <a className="nav-link" href="/postitem">Lost Items</a>
+                    </li>
+                    <li className="nav-item" id="report">
+                        <a className="nav-link" href="/searchitems">Found Items</a>
+                    </li>
+                    <li className="nav-item" id="report">
+                        <a className="nav-link" href="/login">Login</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    );
+}
 
 export default Navbar
