319a876805123d879a7b923bcee79ca963c5e722
Peiji Song <patri.songpj@gmail.com>
2019-06-27 17:00:01 -0400

    add google oAUTH

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,25 +1,36 @@
 import React from "react";
 import {Route,Link} from "react-router-dom"
-// import Loginform from './Loginform';
+import GoogleLogin from 'react-google-login';
 import "./Login.css"
 import Loginform from "./Loginform";
 import Signup from './SignUp';
 
 
-function login(){
+class login extends Component{
+    render(){    
+        const responseGoogle = (response) => {
+            console.log(response);
+            this.signup(response, 'google')
+            }
+
     return(
         <div className="login">
         <h1>Finder's Locker</h1>
             <p>Welcome to finder's locker</p>
             <Link to="/signup">Sign Up</Link>
             <Link to="/loginform">Login</Link>
-            <Link to="/auth/google">Signin With Google</Link>
+            <GoogleLogin
+                clientId="INPUT_GOOGLE_ID"
+                buttonText="Login"
+                onSuccess={responseGoogle}
+                onFailure={responseGoogle}
+                cookiePolicy={'single_host_origin'}
+            />
             <Route path="/loginform" component={Loginform} />
             <Route path="/signup" component={Signup} />
-
-
         </div>
     )
+}
 }
 
 export default login
