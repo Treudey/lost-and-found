45ba892e2cbfd9b57f265d19a92d8ec0eb69fbf1
Peiji Song <patri.songpj@gmail.com>
2019-06-27 14:55:48 -0400

    update login and signup form

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,13 +1,23 @@
 import React from "react";
+import {Route,Link} from "react-router-dom"
+// import Loginform from './Loginform';
+import "./Login.css"
+import Loginform from "./Loginform";
+import Signup from './SignUp';
+
 
 function login(){
     return(
-        <div>
-            <h1>Finder's Locker</h1>
+        <div className="login">
+        <h1>Finder's Locker</h1>
             <p>Welcome to finder's locker</p>
-            <a>Register</a>
-            <a>Login</a>
-            <a>Signin With Google</a>
+            <Link to="/signup">Sign Up</Link>
+            <Link to="/loginform">Login</Link>
+            <Link to="/auth/google">Signin With Google</Link>
+            <Route path="/loginform" component={Loginform} />
+            <Route path="/signup" component={Signup} />
+
+
         </div>
     )
 }
