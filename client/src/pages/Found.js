72e1e11470c7b67a5ed65bffff889ff5a626e5df
amandat <amandatome@me.com>
2019-07-05 15:59:59 -0400

    Mobile Responsiveness

--- client/src/pages/Found.js
+++ client/src/pages/Found.js
@@ -14,173 +14,186 @@ import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import CardActions from '@material-ui/core/CardActions';
 
-
 const backgroundImageFound = 'https://cdn.pixabay.com/photo/2017/09/13/22/25/lost-2747288_1280.png';
 
 //Styles
 const useStyles = makeStyles(theme => ({
-    root: {
-        flexGrow: 1,
-    },
-
-    heroContent: {
-        marginTop: theme.spacing(4),
-        backgroundImage: `url(${backgroundImageFound})`,
-        backgroundPosition: "center",
-        backgroundRepeat: "no-repeat",
-        backgroundSize: "contain",
-        height: '30vh',
-        display: 'flex',
-        flexDirection: 'column',
-        borderBottomColor: "#7FB800",
-        borderBottomStyle: "solid"
-    },
-    h4: {
-        color: "#FF9F1C",
-        paddingBottom: theme.spacing(3)
-    },
-    grid: {
-        marginBottom: "3rem"
-    },
-    container: {
-        // display: 'flex',
-        // flexWrap: 'wrap',
-        marginTop: '5%'
-
-    },
-    textfield: {
-        marginBottom: '10px',
-        padding: theme.spacing(4),
-    },
-    rightIcon: {
+  root: {
+    flexGrow: 1,
+    display: 'flex',
+    flexWrap: 'wrap',
+    flexDirection: "column"
+  },
+  heroContent: {
+    marginTop: theme.spacing(4),
+    backgroundImage: `url(${backgroundImageFound})`,
+    backgroundPosition: "center",
+    backgroundRepeat: "no-repeat",
+    backgroundSize: "contain",
+    height: '30vh',
+    display: 'flex',
+    flexDirection: 'column',
+    borderBottomColor: "#7FB800",
+    borderBottomStyle: "solid",
+  },
+  h4: {
+    color: "#FF9F1C",
+    paddingBottom: theme.spacing(3),
+    textAlign: "center",
+    [theme.breakpoints.between("xs","sm")]: 
+    {
+      paddingBottom: theme.spacing(0)
+  },
+},
+  grid: {
+    marginBottom: "3rem",
+    [theme.breakpoints.between("xs","sm")]: 
+     {
+      marginBottom: theme.spacing(1)
+     },
+  },
+  container: {
+    display: 'flex',
+    flexWrap: 'wrap',
+    marginTop: '5%',
+  },
+  rightIcon: {
+    marginLeft: theme.spacing(1),
+  },
+  button: {
+    marginTop: theme.spacing(1),
+  },
+  sendButton: {
+    backgroundColor: "#7FB800",
+    marginLeft: theme.spacing(2),
+    marginBottom: theme.spacing(1),
+    [theme.breakpoints.between("xs", "sm")]:
+      {
         marginLeft: theme.spacing(1),
-    },
-    button: {
-        marginTop: theme.spacing(1),
-    },
-    sendButton: {
-        backgroundColor: "#7FB800",
-        marginLeft: theme.spacing(2),
-        marginBottom: theme.spacing(2),
-    },
-    cardContent: {
-        marginLeft: theme.spacing(8),
-        marginTop: theme.spacing(2),
-    },
+      }
+  },
+  cardContent: {
+    marginTop: theme.spacing(2),
+  },
+  card: {
+    padding: theme.spacing(2),
+    boxShadow: "2px 2px 2px 2px #555",
+  },
+  textfield: {
+    padding: theme.spacing(1),
+  },
+
+  ///I DON'T USE THESE ANYWHERE
+  // inputInfo: {
+  //   // color:'#fff',
+  // },
+  // inputValue: {
+  //   // color:'#0b2f55',
+  //   // fontWeight:'bold'
+  // },
+  // inputlabel: {
+  //   // color:'rgb(7, 31, 138)',
+  //   marginRight: '10px',
+  //   textShadow: '0 0 0px'
+  // },
 }));
 
 export default function Found() {
-    const classes = useStyles();
-    return (
-        <React.Fragment>
-            <div className={classes.heroContent}></div>
-            <Container className={classes.container}>
-                <Grid container className={classes.grid}>
-                    <Grid item md={8}>
-                        <Card>
-                            <CardContent className={classes.cardContent}>
-                                <Typography className={classes.h4} component="h1" variant="h4" align="left" gutterBottom>
-                                    Tell us a bit about the item you found
-                                </Typography>
-                                <form noValidate autoComplete="off">
-                                <Grid container className={classes.grid}>
-                                        <Grid item md={11}>
-                                            <TextField
-                                                id="standard-full-width"
-                                                htmlFor="titleItem"
-                                                placeholder="1. Please provide a title for the item you found"
-                                                helperText="Please provide a title for the item you found"
-                                                fullWidth
-                                                required
-                                                aria-describedby="location-helper-text"
-                                                InputLabelProps={{
-                                                    shrink: true
-                                                }}
-                                            />
-                                        </Grid>
-                                    </Grid>
-                                    <Grid container className={classes.grid}>
-                                        <Grid item md={11}>
-                                            <TextField
-                                                id="standard-full-width"
-                                                htmlFor="locationItem"
-                                                placeholder="2. Please indicate the location where the item was found"
-                                                helperText="Please share the rough location or main intersection where you found the item"
-                                                fullWidth
-                                                required
-                                                aria-describedby="location-helper-text"
-                                                InputLabelProps={{
-                                                    shrink: true,
-                                                }}
-                                            />
-                                        </Grid>
-                                    </Grid>
-                                    <Grid container className={classes.grid}>
-                                        <Grid item md={11}>
-                                            <TextField
-                                                id="standard-full-width"
-                                                htmlFor="colorItem"
-                                                placeholder="3. Please provide the colour(s) of the item"
-                                                helperText="Please share the colour(s) of the item you found"
-                                                fullWidth
-                                                required
-                                                aria-describedby="location-helper-text"
-                                                InputLabelProps={{
-                                                    shrink: true,
-                                                }}
-                                            />
-                                        </Grid>
-                                    </Grid>
-                                    <Grid container className={classes.grid}>
-                                        <Grid item md={11}>
-                                            <TextField
-                                                id="standard-full-width"
-                                                htmlFor="descriptionItem"
-                                                placeholder="4. Please provide a description of the item"
-                                                helperText="Please give us little discription about the item you found"
-                                                fullWidth
-                                                required
-                                                multiline
-                                                aria-describedby="location-helper-text"
-                                                InputLabelProps={{
-                                                    shrink: true,
-                                                }}
-                                            />
-                                        </Grid>
-                                    </Grid>
-                                    <Grid container className={classes.grid}>
-                                        <Grid item md={11}>
-                                            <TextField
-                                                id="standard-full-width"
-                                                placeholder="4. Please upload the image of the item you found in here"
-                                                helperText="Please upload the image of the item you found in here"
-                                                fullWidth
-                                                aria-describedby="location-helper-text"
-                                                InputLabelProps={{
-                                                    shrink: true,
-                                                }}
-                                                accept="image/*"
-                                                className={classes.input}
-                                                multiple
-                                                type="file"
-                                            />
-                                            <Button variant="contained" color="default" className={classes.button} component="span">
-                                                Upload
-                                <CloudUploadIcon className={classes.rightIcon} />
-                                            </Button>
-                                        </Grid>
-                                    </Grid>
-                                </form>
-                            </CardContent>
-                            <CardActions>
-                                <Button className={classes.sendButton} variant="contained" color="#7FB800" >Send
-                                    <Icon className={classes.rightIcon}>send</Icon>
-                                </Button>
-                            </CardActions>
-                        </Card>
-                    </Grid>
-                </Grid>
-            </Container>
-        </React.Fragment>
-    );
+  const classes = useStyles();
+  return (
+    <React.Fragment>
+      <div className={classes.heroContent}></div>
+      <Container className={classes.container}>
+        <Grid container className={classes.grid}>
+          <Grid item md={12} sm={12} xs={12}>
+            <Card>
+              <CardContent className={classes.cardContent}>
+                <Typography className={classes.h4} component="h1" variant="h4" align="left" gutterBottom>
+                  Tell us a bit about the item you lost
+                  </Typography>
+                <form noValidate autoComplete="off">
+                  <Grid item md={12} sm={12} xs={12} className={classes.grid}>
+                    <TextField
+                      className={classes.textfield}
+                      id="titleItemField"
+                      htmlFor="titleItem"
+                      label="1. Please provide a title for the item you lost"
+                      helperText="Please provide a title for the item you lost"
+                      fullWidth
+                      required
+                      aria-describedby="title-found-helper-text"
+                    />
+                  </Grid>
+                  <Grid item md={12} sm={12} xs={12} className={classes.grid}>
+                    <TextField
+                      className={classes.textfield}
+                      id="locationItemField"
+                      htmlFor="locationItem"
+                      label="2. Please indicate the location where the item was found"
+                      helperText="Please share the rough location or main intersection where you found the item"
+                      fullWidth={true}
+                      required={true}
+                      aria-describedby="location-found-helper-text"
+                    />
+                  </Grid>
+                  <Grid item md={12} sm={12} xs={12} className={classes.grid}>
+                    <TextField
+                      className={classes.textfield}
+                      id="colorItemField"
+                      htmlFor="colorItem"
+                      label="3. Please provide the colour(s) of the item"
+                      helperText="Please share the colour(s) of the item you found"
+                      fullWidth={true}
+                      required={true}
+                      aria-describedby="color-found-helper-text"
+                    />
+                  </Grid>
+                  <Grid item md={12} sm={12} xs={12} className={classes.grid}>
+                    <TextField
+                      id="descriptionItemField"
+                      htmlFor="descriptionItem"
+                      label="4. Please provide a description of the item"
+                      helperText="Please give us little description about the item you found"
+                      fullWidth={true}
+                      required={true}
+                      multiline={true}
+                      rows="3"
+                      aria-describedby="description-found-helper-text"
+                      margin="normal"
+                    />
+                  </Grid>
+                  <Grid item md={12} sm={12} xs={12} className={classes.grid}>
+                    <TextField
+                      className={classes.textfield}
+                      id="imageItemField"
+                      label="5. Please upload the image of the item you found in here"
+                      helperText="Please upload the image of the item you found in here"
+                      fullWidth={true}
+                      disabled={true}
+                      aria-describedby="image-found-helper-text"
+                      InputLabelProps={{
+                        shrink: true,
+                      }}
+                      accept="image/*"
+                      multiple
+                      type="file"
+                    />
+                    <Button variant="contained" color="default" className={classes.button} component="span">
+                      Upload
+                        <CloudUploadIcon className={classes.rightIcon} />
+                    </Button>
+                  </Grid>
+                </form>
+              </CardContent>
+              <CardActions>
+                <Button className={classes.sendButton} variant="contained" color="#7FB800" >Send
+                      <Icon className={classes.rightIcon}>send</Icon>
+                </Button>
+              </CardActions>
+            </Card>
+          </Grid>
+        </Grid>
+      </Container>
+    </React.Fragment>
+  );
 }
