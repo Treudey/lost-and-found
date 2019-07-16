e4e56b57e0d35cb762b37f0863b02108a550ddfb
Treudey <alextreude@hotmail.com>
2019-07-15 21:38:30 -0400

    changed name of route

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(express.json({ extended: false }));
 
 
 // Define Routes
-app.use('/api/users',require('./routes/users'))
+app.use('/api/users',require('./routes/Users'))
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/profiles', require('./routes/profiles'));
 app.use('/api/found',require('./routes/found'))
